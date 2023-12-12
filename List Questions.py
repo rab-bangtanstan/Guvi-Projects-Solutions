2497f91306ee5f9a356ac20f0c511ecade12c160
rab-bangtanstan <68557893+rab-bangtanstan@users.noreply.github.com>
2023-12-12 11:09:56 +0530

    Add files via upload

---
+++ List Questions.py
@@ -0,0 +1,70 @@
+# Find the maximum number in a list
+
+# Find the minimum number in a list
+
+
+
+# Create an empty list called my_list.
+# Add integers from 1 to 10 (inclusive) to the list.
+# Print the list.
+# Remove the even numbers from the list.
+# Print the modified list.
+
+# listx = []
+#
+# for i in range(1,11):
+#     listx.append(i)
+#
+# print(listx)
+#
+# for i in listx:
+#     if i % 2 == 0:
+#         listx.remove(i)
+#
+# print(listx)
+
+
+# Create a list with custom range from the user eg : 5
+# collect the input from the user as well and store it in the list
+# calculate the sum of all inputs in the list and print the result
+
+
+
+# Create a list with user inputs
+# create another list from the original list to store even numbers only
+# similarly create another list to store the odd numbers
+
+
+# Create a list original_list containing integers from 1 to 20.
+# Use slicing to extract the following sublists:
+# Elements from index 5 to index 15 (inclusive).
+# Elements from index 0 to index 10 (exclusive).
+# Elements starting from index 15 to the end of the list.
+# Print all the extracted sublists.
+
+
+# Create a list with 10 names as values
+# The user will give a name, check if the name is present in the list or not
+# if present , remove the name and print the name is successfully removed
+# if not present, print the name is not present in the list
+
+
+# Write a program to find the first occurrence of a number divisible by 7 and stop the loop
+# Print all numbers from 1 to 10 except multiples of 3
+
+# Create a program that checks if a number is odd and prints the number. If it's even, do nothing
+
+# Write a program to check if a letter is present in a string or not
+
+
+word = "Word"
+
+# for i in word:
+#     if i == 'o':
+#         print('o is present in the string')
+#
+# i = 'o'
+#
+# print(i in word)
+
+
