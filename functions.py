4cc4d7e70dfcf167d72979155990af6efc0e317e
rab-bangtanstan <68557893+rab-bangtanstan@users.noreply.github.com>
2023-12-12 11:05:19 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,26 @@
+
+
+
+
+# # Function Definition
+# def display_message(var_name): # function will catch the arguments---> parameters or inputs
+#     # name = "Adhavan"
+#     print("Hi ", var_name, " Welcome to pycharm")
+#
+#
+# # Calling your function / Function call
+# display_message("Alice") # passing the value to your function
+
+
+# Adding two numbers
+
+def add_numbers(num1,num2):
+    sum1 = num1 + num2
+    return sum1 # it will return the sum value back to caller
+
+
+print( add_numbers(2,4) )
+#  var = 6
+
+
+
