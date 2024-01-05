76a1dd696c9d5bd34315ec7b680219c532cea6be
Rabiya Begam <68557893+rab-bangtanstan@users.noreply.github.com>
2024-01-05 09:06:21 +0530

    Add files via upload
    Project Question : Write a python program to calculate the multiplication tables for a user given number.

---
+++ Multiplication Table.py
@@ -0,0 +1,8 @@
+
+
+input_number = int(input("Enter the number for which the multiplication table will be calculated:"))
+
+print("Multiplication Table")
+for i in range(1,13):
+    print(input_number,"X",i,"=",input_number*i)
+    
