6ff16bc56f81ad22a646ce32c093f100d6f3107b
Rabiya Begam <68557893+rab-bangtanstan@users.noreply.github.com>
2024-09-23 10:35:43 +0530

    Create PR13-PyScreenshot.py

---
+++ PR13-PyScreenshot.py
@@ -0,0 +1,7 @@
+import pyscreenshot
+
+# Take a screenshot using Pillow for better image handling
+screenshot = pyscreenshot.grab()
+screenshot.save("screenshot3.png")
+
+print("Screenshot saved as screenshot2.png")
